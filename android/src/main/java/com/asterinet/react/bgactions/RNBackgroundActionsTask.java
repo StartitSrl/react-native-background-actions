6c6df08598b048d99ea6df4e13b6733f331eb059
StartitSrl <startitnetsolutions@gmail.com>
2025-02-06 09:59:28 +0100

    Fixed MUTABLE flag crash for android 14+

--- android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
+++ android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
@@ -44,7 +44,7 @@ final public class RNBackgroundActionsTask extends HeadlessJsTaskService {
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             contentIntent = PendingIntent.getActivity(context,0, notificationIntent, PendingIntent.FLAG_MUTABLE | PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_MUTABLE);
+            contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         } else {
